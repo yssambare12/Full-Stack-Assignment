2c5b1a4815cab1f8ee9e4972535899f28d4ae545
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2022-02-20 12:37:15 +0530

    Create Header.jsx

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-left-group">
+        <div className="logo-div">
+            <Link to="/">
+         
+          </Link>
+        </div>
+        <div className="home-div">
+        </div>
+        <div className="podcast-div">
+        </div>
+      </div>
+        <div className="user-div">
+      <a href="https://www.mongodb.com/basics/clusters/mongodb-cluster-setup">
+          <h4>signin</h4>
+      </a>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
